2894005e0f1dfecb9033764cd57a03565c499caf
r220 <upontime000@gmail.com>
2025-07-08 17:36:50 +0300

    editing

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,9 @@ AOS.init();
 
 const hero_nav = document.querySelector(".hero-nav");
 const hero_background = document.querySelector(".hero-background");
+const hero_content = document.querySelector('.hero-content');
 const about_section = document.querySelector("#about");
+const header = document.querySelector('header');
 
 // Access the first stylesheet
 const stylesheet = document.styleSheets[5];
@@ -16,25 +18,25 @@ window.onscroll = function() {
     hero_background.style.transform = `scale(${value})`;
 
   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
-    document.querySelector('.hero-content').style.opacity = 0;
-    document.querySelector('.hero-content').style.transform = 'translate3d(0,-50px,0)';
+    hero_content.style.opacity = 0;
+    hero_content.style.transform = 'translate3d(0,-50px,0)';
     hero_nav.style.transform = 'translate3d(0,-50px,0)';
     stylesheet.insertRule( '.hero-background::after { content: ""; background-color: rgb(238 234 251 / 90%);; }',
       stylesheet.cssRules.length
     );
     
   } else {
-    document.querySelector('.hero-content').style.opacity = 1;
-    document.querySelector('.hero-content').style.transform = 'none';
+    hero_content.style.opacity = 1;
+    hero_content.style.transform = 'none';
     hero_nav.style.transform = 'translate3d(0,0,0)';
     stylesheet.insertRule( '.hero-background::after { content: ""; background-color: rgb(238 234 251 / 60%);; }',
       stylesheet.cssRules.length
     );
   }
   if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
-    document.getElementById("navbar").style.top = "1rem";
+    header.style.top = "1rem";
   } else {
-    document.getElementById("navbar").style.top = "-4rem";
+    header.style.top = "-4rem";
   }
 };
 
